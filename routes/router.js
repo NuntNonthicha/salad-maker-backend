a89ff7d27ebb684c2fb79e7d09ec919c151b9818
NuntNonthicha <nunt.nonthicha@gmail.com>
2024-08-02 22:27:16 +0700

    fix: rm userController

--- routes/router.js
+++ routes/router.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const useIngredientRoute = require("./ingredient.route");
 const useRecipeRoute = require("./recipe.route");
-const useUserRoute = require("./user.route");
 
 const Router = express.Router();
 
 useIngredientRoute(Router);
 useRecipeRoute(Router);
-useUserRoute(Router);
 
 module.exports = Router;
